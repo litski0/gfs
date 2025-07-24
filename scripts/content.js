97aca33c24dce3673e02ae544f0eb99e18f2a98f
litski0 <devanshladdha2006@gmail.com>
2025-07-24 12:08:51 +0530

    v1.5.0

--- scripts/content.js
+++ scripts/content.js
@@ -1,23 +1,34 @@
+
 let key="";
 let secret="";
 const contestId=parseInt(location.href.split("/")[5])
 const index=location.href.split("/")[6]
 friends_submission=[];
+storagekey=`${contestId}[${index}]`;
+console.log(storagekey)
 // Get key and secret 
-if (chrome.storage.local.get(['apiKey','apiSecret'],(result)=>{
-        if(result.apiKey && result.apiSecret){
+function main(){
+console.log(chrome.storage.local.getKeys());
+if (chrome.storage.local.get(['apiKey','apiSecret',`${storagekey}`],(result)=>{
+        console.log(result[storagekey]);
+        if(result.apiKey && result.apiSecret && !result[storagekey]){
             key=result.apiKey;
             secret=result.apiSecret;
             getFriends();
             }
-            else{
+            else if (!result.apiKey || !result.apiSecret){
               console.log("Key and Secret has not been set");
             }
+            else if (result[storagekey]){
+              friends_submission=result[storagekey];
+              changeHtml(cache=true);
+            }
     }));
-
+  }
 
 
 async function getFriends(){
+  
   const time=getCurrentTimestamp();
   const random=getRandom();
   const hash= await getHash(random,time);
@@ -29,22 +40,8 @@ async function getFriends(){
     throw new Error(response.comment)
   }
   const friends=response.result;
-  const gpnode=document.createElement("div");
-  gpnode.innerHTML=`
-      <div class="roundbox sidebox borderTopRound" id="extensionDiv"style="">
-          <div class="caption titled">→ Friends' Correct Submission <br>(From Extension)
-          </div>
-          <table class="rtable smaller">
-              <tbody id="tbodyExtension">
-                <tr id="messageExtension">
-                  <td>
-                    Waiting for Data to be fetched...
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>`
-  document.querySelectorAll("div.roundbox.sidebox.borderTopRound")[document.querySelectorAll("div.roundbox.sidebox.borderTopRound").length-2].insertAdjacentElement("beforebegin",gpnode);
+  // 
+  createTable();
   getSubmission(friends);
   }
   catch(error)
@@ -81,7 +78,7 @@ async function getSubmission(friends){
           if (submission.contestId===contestId){
             if (submission.problem.index===index && submission.verdict==="OK"){
             
-              friends_submission.push([element,submission.id])
+              friends_submission.push([element,submission.id,"black"])
               
             }
           }
@@ -97,11 +94,18 @@ async function getSubmission(friends){
 };
     
 
-function changeHtml(){
+async function changeHtml(cache=false){
   if (friends_submission.length===0){
-    document.querySelector("#messageExtension").innerHTML="<strong>Sorry, None of Friends submitted code which was Accepted</strong>";
+    document.querySelector("#messageExtension").innerHTML="<strong>Sorry, None of Friends submitted code which was Accepted</strong><br><button id='refresh'>Refresh List</button>";
+    
   }
   else{
+    if (!cache){
+    friends_submission=await getColor(friends_submission);
+    }
+    else{
+      createTable()
+    }
     table=document.querySelector("#tbodyExtension");
     table.innerHTML=`
         <tr>
@@ -109,17 +113,33 @@ function changeHtml(){
           <th class=" " style="width:7em;">Submission</th>
         </tr>`
     
-    friends_submission.forEach(element => {
+    
+      for (element of friends_submission){
       node=document.createElement("tr");
       node.innerHTML=`
-          <td class="bottom " style="border: 1px solid rgb(185, 185, 185); border-left:none;">${element[0]}</td>
+          <td class="bottom user-${element[2]}" style="border: 1px solid rgb(185, 185, 185); border-left:none;"><strong>${element[0]}</strong></td>
           <td class="left bottom" style="border:1px solid rgb(185, 185, 185);border-right:none;"><a href="/contest/${contestId}/submission/${element[1]}">${element[1]}</a></td>`      
       table.appendChild(node);
-    });
+      }
+      node=document.createElement("tr");
+      node.innerHTML=`<td colspan="2"><button id="refresh">Refresh List</button></td>`
+      table.appendChild(node);
+      
+
+
+  }
+        document.querySelector('#refresh').addEventListener('click',()=>{
+        chrome.storage.local.remove([`${storagekey}`]);
+            document.querySelector("#extensionDiv").remove()
+            friends_submission=[]
+            main()
+
+          
+      })
   }
   
 
-}
+
 function getCurrentTimestamp () {
   return parseInt(Date.now()/1000)
 }
@@ -139,3 +159,76 @@ async function getHash(random,time){
     return hashHex;
 
 }
+
+async function getColor(friends_submission){
+  for (element of friends_submission){
+   try {  
+      response =await fetch (`https://codeforces.com/api/user.info?handles=${element[0]}&checkHistoricHandles=false`)
+      response = await response.json()
+      if (response.status!='OK'){
+        throw new Error (response.comment)
+      }
+      rank=response.result[0].rank;
+      rColor='';
+
+      if (rank==='newbie'){
+        rColor='gray'
+      }
+      else if (rank === "pupil"){
+        rColor='green'
+      }
+      else if (rank==="specialist"){
+        rColor='cyan'
+      }
+      else if (rank==="expert"){
+        rColor='blue'
+      }
+      else if (rating ==="candidate master"){
+        rColor='violet'
+      }
+      else if (rating==="master" || rating === "international master"){
+        rColor='orange'
+      }
+      else if (rating === "grandmaster" || rating === "international grandmaster"){
+        rColor='red'
+      }
+      else if (rating === "legendary grandmaster"){
+        rColor="legendary"
+      }
+    element[2]=rColor      
+
+   } 
+    catch(error){
+
+        console.log(error);
+        
+      }   
+  }
+
+  chrome.storage.local.set(
+                {[storagekey]:friends_submission,
+            },()=> {console.log("setted data")});
+  return friends_submission
+  
+}
+
+function createTable (){
+  const gpnode=document.createElement("div");
+  gpnode.innerHTML=`
+      <div class="roundbox sidebox borderTopRound" id="extensionDiv"style="">
+          <div class="caption titled">→ Friends' Correct Submission <br>(From Extension)
+          </div>
+          <table class="rtable smaller">
+              <tbody id="tbodyExtension">
+                <tr id="messageExtension">
+                  <td>
+                    Waiting for Data to be fetched...
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>`
+  document.querySelectorAll("div.roundbox.sidebox.borderTopRound")[document.querySelectorAll("div.roundbox.sidebox.borderTopRound").length-2].insertAdjacentElement("beforebegin",gpnode);
+}
+
+main()
